94287ca97241a720f9ee56407a8f796badae1aca
Maarten Hermans <maarten@mhermans.net>
2019-12-30 21:23:20 +0100

    add lights on delay timer

--- stairlights/stairlights.py
+++ stairlights/stairlights.py
@@ -12,6 +12,11 @@ a_in_light = AnalogIn(board.A2)
 # Neopixel strip on PIN D5
 pixel_pin = board.D5
 
+LIGHT_CUTOFF = 1.0     # value 0-100
+MOVEMENT_CUTOFF = 95.0 # value 0-100
+LIGHT_ON_DELAY = 10    # time lights stays on in seconds
+
+
 num_pixels = 30
 ORDER = neopixel.GRB
 pixels = neopixel.NeoPixel(
@@ -19,34 +24,55 @@ pixels = neopixel.NeoPixel(
     brightness=0.3, auto_write=False,
     pixel_order=ORDER)
 
-def is_dark(light_sensor, cutoff=0.3):
-    # analog pin max value is 65535
+def is_dark(light_sensor, cutoff=1.0):
+    # analog pin max value is 65535 => ~1-100
+    # [~4 is ambient room light, ~96 directly under lamp]
     light_pct = (light_sensor.value / 65535)*100
     if light_pct < cutoff:
         return(True)
     else:
         return(False)
 
-def is_movement(pir_sensor, cutoff=0.9):
-    #  analog pin max value is 65535
+def is_movement(pir_sensor, cutoff=95):
+    #  analog pin max value is 65535 => range [~0.2 - 100]
+    #  0.1-0.2 is no movement, 98-99 is movement
     movement_pct = (pir_sensor.value / 65535)*100
+    print(movement_pct)
     if movement_pct > cutoff:
         return(True)
     else:
         return(False)
 
+
+TS_BOOT = time.monotonic()
+TS_LAST_MOVEMENT = 0
+
 while True:
-    if is_dark(a_in_light, 0.30):
-        if is_movement(a_in_pir, 0.90):
-            pixels.fill((255, 0, 0))
-            pixels.show()
-        else:
-            pixels.fill((0, 255, 0))
-            pixels.show()
-    else:
-        pixels.fill((0, 0, 255))
+    if not is_dark(a_in_light, LIGHT_CUTOFF):
+        # ambient light present -> turn of pixels
+        pixels.fill((0, 0, 0))
         pixels.show()
 
-    #print(is_movement(a_in_pir, 0.90))
-    #print(is_dark(a_in_light, 0.30))
-    #time.sleep(0.1)
+    else:
+        # dark and movement
+        if is_movement(a_in_pir, MOVEMENT_CUTOFF) and not TS_LAST_MOVEMENT:
+            # first movement -> activate pixels and set timestamp
+            TS_LAST_MOVEMENT = time.monotonic()
+            pixels.fill((255, 0, 0))
+            pixels.show()
+            
+        elif is_movement(a_in_pir, MOVEMENT_CUTOFF) and  ( (time.monotonic() - TS_LAST_MOVEMENT) < LIGHT_ON_DELAY ):    
+            # again movement -> reset timestamp
+            TS_LAST_MOVEMENT = time.monotonic()
+            pixels.fill((255, 0, 0))
+            pixels.show()
+            
+        elif (time.monotonic() - TS_LAST_MOVEMENT) < LIGHT_ON_DELAY:
+            pixels.fill((0, 0, 255))
+            pixels.show()
+        
+        # dark and no movement
+        else:
+            TS_LAST_MOVEMENT = 0
+            pixels.fill((0, 255, 0))
+            pixels.show()
